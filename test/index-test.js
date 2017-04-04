9d636f4ca082f0fbac5dfcd3135aaf7926b9684c
CUREST TREBOW <43941751+curest0x1021@users.noreply.github.com>
2017-04-04 11:01:35 -0400

    Fix test 1

--- test/index-test.js
+++ test/index-test.js
@@ -4,7 +4,7 @@ describe('Oldercoaster', function () {
   });
 
   it('should have been created using `React.createClass()`', function () {
-    expect(OlderCoaster.name).toExist('The `Oldercoaster` component is being created using a ES2015 class.');
+    expect(OlderCoaster.name).toNotExist('The `Oldercoaster` component is being created using a ES2015 class.');
   });
 
   it('should have the right DOM markup', function () {
@@ -39,4 +39,3 @@ describe('ButcherShop', function () {
     expect(this.wrapper.html()).toEqual(`<div class="butcher-shop"><p>Hello! We have the following products for sale today:</p><ul><li>Tenderloin</li><li>Short ribs</li><li>Beef shin</li><li>Ribeye</li></ul></div>`);
   });
 });
-
