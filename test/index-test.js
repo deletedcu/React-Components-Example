c6d15f3985a0509a8087f4094f84361239f67f91
CUREST TREBOW <43941751+curest0x1021@users.noreply.github.com>
2017-06-02 08:13:11 -0400

    Update tests to work with ES6 and chai

--- test/index-test.js
+++ test/index-test.js
@@ -1,41 +1,56 @@
-describe('OlderCoaster', function () {
-  before(function () {
-    this.wrapper = enzyme.shallow(React.createElement(OlderCoaster));
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { OlderCoaster, InFrontOfYou, ButcherShop } from '../src/index'
+
+describe('OlderCoaster', () => {
+  let wrapper;
+
+  before(() => {
+    wrapper = shallow(React.createElement(OlderCoaster));
   });
 
-  it('should have been created using `React.createClass()`', function () {
-    expect(OlderCoaster.name).toNotExist('The `OlderCoaster` component is being created using a ES2015 class.');
+  it('renders withouth crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<OlderCoaster />, div);
   });
 
-  it('should have the right DOM markup', function () {
-    expect(this.wrapper.html()).toEqual(`<div class="oldercoaster"><p>Two grannies having the time of their life!</p><p>Passengers:</p><ul><li>Agnes</li><li>Muriel</li></ul></div>`);
-  });
-});
-
-describe('InFrontOfYou', function () {
-  before(function () {
-    this.wrapper = enzyme.shallow(React.createElement(InFrontOfYou));
-  });
-
-  it('should have been created using ES2015 class syntax`', function () {
-    expect(InFrontOfYou.name).toExist('The `InFrontOfYou` component is not being created using a ES2015 class.');
-  });
-
-  it('should have the right DOM markup', function () {
-    expect(this.wrapper.html()).toEqual(`<div><p>You shouldn&#x27;t look too far.</p><p>Sometimes, the solution is <strong>right in front of you.</strong></p></div>`);
+  it('should have the right DOM markup', () => {
+    expect(wrapper.html()).to.equal(`<div class="oldercoaster"><p>Two grannies having the time of their life!</p><p>Passengers:</p><ul><li>Agnes</li><li>Muriel</li></ul></div>`);
   });
 });
 
-describe('ButcherShop', function () {
-  before(function () {
-    this.wrapper = enzyme.shallow(React.createElement(ButcherShop));
+describe('InFrontOfYou', () => {
+  let wrapper;
+
+  before(() => {
+    wrapper = shallow(React.createElement(InFrontOfYou));
   });
 
-  it('should have been created using ES2015 class syntax`', function () {
-    expect(ButcherShop.name).toExist('The `ButcherShop` component is not being created using a ES2015 class.');
+  it('renders withouth crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<InFrontOfYou />, div);
   });
 
-  it('should have the right DOM markup', function () {
-    expect(this.wrapper.html()).toEqual(`<div class="butcher-shop"><p>Hello! We have the following products for sale today:</p><ul><li>Tenderloin</li><li>Short ribs</li><li>Beef shin</li><li>Ribeye</li></ul></div>`);
+  it('should have the right DOM markup', () => {
+    expect(wrapper.html()).to.equal(`<div><p>You shouldn&#x27;t look too far.</p><p>Sometimes, the solution is right in front of you.</p></div>`);
+  });
+});
+
+describe('ButcherShop', () => {
+  let wrapper;
+
+  before(() => {
+    wrapper = shallow(React.createElement(ButcherShop));
+  });
+
+  it('renders withouth crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<ButcherShop />, div);
+  });
+
+  it('should have the right DOM markup', () => {
+    expect(wrapper.html()).to.equal(`<div class="butcher-shop"><p>Hello! We have the following products for sale today:</p><ul><li>Tenderloin</li><li>Short ribs</li><li>Beef shin</li><li>Ribeye</li></ul></div>`);
   });
 });
