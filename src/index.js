c81081990e75fcd4c13a7fa2127a3d51cb66649e
CUREST TREBOW <43941751+curest0x1021@users.noreply.github.com>
2017-06-02 07:41:13 -0400

    Move index.js file to src directory

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
+// Make sure to import React and ReactDOM
+
 const BUTCHER_PRODUCTS = [
   'Tenderloin',
   'Short ribs',
   'Beef shin',
   'Ribeye'
 ];
+
+// Define these exported consts to be Components
+export const OlderCoaster = undefined;
+export const InFrontOfYou = undefined;
+export const ButcherShop = undefined;
 
 ReactDOM.render(
   React.createElement('div', {}, [
