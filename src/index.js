4afedb53dfd63bc5c8115905ec1f32ab42bac60d
CUREST TREBOW <43941751+curest0x1021@users.noreply.github.com>
2017-10-13 10:04:59 -0400

    updated babel to stage-1, switched root to global

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// Make sure to import React and ReactDOM
 
 const BUTCHER_PRODUCTS = [
   'Tenderloin',
@@ -8,38 +7,10 @@ const BUTCHER_PRODUCTS = [
   'Ribeye'
 ];
 
-export class OlderCoaster extends React.Component {
-  render() {
-    return React.createElement('div', { className: 'oldercoaster' }, [
-      React.createElement('p', {}, 'Two grannies having the time of their life!'),
-      React.createElement('p', {}, 'Passengers:'),
-      React.createElement('ul', {}, [
-        React.createElement('li', {}, 'Agnes'),
-        React.createElement('li', {}, 'Muriel')
-      ])
-    ]);
-  }
-}
-
-export class InFrontOfYou extends React.Component {
-  render() {
-    return React.createElement('div', {}, [
-      React.createElement('p', {}, `You shouldn't look too far.`),
-      React.createElement('p', {}, 'Sometimes, the solution is right in front of you.')
-    ]);
-  }
-}
-
-export class ButcherShop extends React.Component {
-  render() {
-    return React.createElement('div', { className: 'butcher-shop' }, [
-      React.createElement('p', {}, 'Hello! We have the following products for sale today:'),
-      React.createElement('ul', {},
-        BUTCHER_PRODUCTS.map(product => React.createElement('li', {}, product))
-      )
-    ]);
-  }
-}
+// Define these exported classes
+export class OlderCoaster extends React.Component {};
+export class InFrontOfYou extends React.Component {};
+export class ButcherShop extends React.Component {};
 
 ReactDOM.render(
   React.createElement('div', {}, [
