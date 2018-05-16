7f936ac5326f0015f6416ea70cef31b6f516b14f
CUREST TREBOW <43941751+curest0x1021@users.noreply.github.com>
2018-05-16 15:43:33 -0400

    added explicit import notes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+// import React from 'react' // We need to import react so we can make use of its .component class
+// import ReactDOM from 'react-dom' // ...and we need to import ReactDOM so we can create and test a virtual DOM with react!
+
 const BUTCHER_PRODUCTS = [
   'Tenderloin',
   'Short ribs',
